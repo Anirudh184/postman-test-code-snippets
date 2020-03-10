0334ae5149f31a1cd8f841df6a6cb380d5cb2ea8
Anirudh184 <34640790+Anirudh184@users.noreply.github.com>
2020-03-10 18:45:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*For configuting the test variable if the status code is 200*/
 if(pm.response.code == 201) {
     console.log(pm.response.json().token)
     pm.environment.set('authToken', pm.response.json().token);
